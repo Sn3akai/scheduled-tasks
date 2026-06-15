5e5a2c8088be358ea1980f4e13b884b158ea6144
Sn3akai <126552552+Sn3akai@users.noreply.github.com>
2026-06-15 18:52:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from random import randint
 import smtplib
 
 my_email = os.environ.get("MY_EMAIL")
-password = os.environ.get("MY_PASSWORD")
+password = os.environ.get("PASSWORD")
 
 
 now = dt.datetime.now()
