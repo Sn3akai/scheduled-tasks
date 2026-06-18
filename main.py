12b29ecc12c0b2d0cef32205a4d00c6fe2afd023
Sn3akai <126552552+Sn3akai@users.noreply.github.com>
2026-06-19 07:43:08 +0800

    Update main.py
    changed to ISS detector

--- main.py
+++ main.py
@@ -1,35 +1,71 @@
-import datetime as dt
-import pandas as pd
-from random import randint
+import datetime
 import smtplib
 import os
+import requests
+from datetime import datetime as dt
+
 
 my_email = os.environ.get("MY_EMAIL")
 password = os.environ.get("MY_PASSWORD")
 
+MY_LAT = 14.675461
+MY_LONG = 121.114812
 
-now = dt.datetime.now()
-today_month = now.month
-today = now.day
-##################### Extra Hard Starting Project ######################
+iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+iss_response.raise_for_status()
+iss_data = iss_response.json()
 
-birth_data = pd.read_csv("birthdays.csv")
-all_birthdays = birth_data.to_dict(orient="records")
+iss_longitude = float(iss_data["iss_position"]["longitude"])
+iss_latitude = float(iss_data["iss_position"]["latitude"])
 
-for birthday_checker in all_birthdays:
-    if birthday_checker["month"] == today_month and birthday_checker["day"] == today:
-        with open(f"letter_templates/letter_{randint(1, 3)}.txt") as greeting_file:
-            template = greeting_file.read()
-        clean_name = birthday_checker["name"].strip()
-        new_letter = template.replace("[NAME]", clean_name)
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs=birthday_checker["email"],
-                msg=f"Subject:Happy Birthday\n\n{new_letter}"
-            )
+iss_position = (iss_longitude,iss_latitude)
 
 
+parameters = {
+    "lat":MY_LAT,
+    "lng":MY_LONG,
+    "max_lat": MY_LAT + 0.07,
+    "min_lat": MY_LAT - 0.07,
+    "max_lng": MY_LONG + 0.07,
+    "min_lng": MY_LONG - 0.07,
+    "formatted":0
+}
 
+
+
+print(f"LAT: {parameters["lat"]}")
+print(f"LNG: {parameters["lng"]}")
+
+sun_response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+sun_response.raise_for_status()
+
+sun_data = sun_response.json()
+
+sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
+time_now = dt.now(datetime.UTC).hour
+print(f"SUNRISE: {sunrise}")
+print(f"SUNSET: {sunset}")
+print(f"CURRENT TIME: {time_now}")
+
+#If the ISS is close to my current position
+#and it is currently dark
+#then send me an email to tell me to look up.
+#BONUS: run the code every 60 seconds
+
+if (parameters["max_lat"] >= iss_latitude >= parameters["min_lat"] and
+        parameters["max_lng"] >= iss_longitude >= parameters["min_lng"] and
+        (time_now >= sunset or time_now <= sunrise)):
+    print("The ISS is nearby. LOOK UP!")
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="kcmsn.ccna@gmail.com",
+            msg=f"Subject: ISS IS NEARBY!\n\n"
+                f"Look up! The ISS is nearby.\n"
+                f"Current location:\n"
+                f"Latitude: {iss_latitude}\n"
+                f"Longitude: {iss_longitude}\n"
+        )
